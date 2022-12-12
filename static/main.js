20780f933338ef403052edd1a3dd4dce8009d4bf
Vinnie <santoscoyvinnie@yahoo.com>
2022-12-12 15:05:27 +0900

    additional files

---
+++ static/main.js
@@ -0,0 +1,16 @@
+function openForm() {
+  document.getElementById("myForm").style.display = "block";
+  }
+  
+function closeForm() {
+  document.getElementById("myForm").style.display = "none";
+  }
+
+function openEditForm(tmr_info) {
+  document.getElementById("myEditForm").style.display = "block";
+  console.log('hello world')
+  }
+  
+function closeEditForm() {
+  document.getElementById("myEditForm").style.display = "none";
+  }
